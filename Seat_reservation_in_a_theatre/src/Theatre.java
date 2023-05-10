cff3f91079b9784dab08f40e8342637b17c8e9f7
Mohamed Abdelfattah <121400510+MohamedAbdelfattah022@users.noreply.github.com>
2023-05-10 19:32:38 +0300

    Add files via upload

---
+++ Seat_reservation_in_a_theatre/src/Theatre.java
@@ -0,0 +1,20 @@
+public class Theatre {
+    private Seat[][] firstClassSeats;
+    private Seat[][] secondClassSeats;
+    private Seat[][] thirdClassSeats;
+
+    public boolean reserveSeat(int seatClass, int row, int column) {
+        return false;
+    }
+
+    public boolean cancelReservation(int seatClass, int row, int column) {
+        return false;
+    }
+
+    public void resetReservations() {
+    }
+
+    public String getSeatStatus(int seatClass) {
+        return "";
+    }
+}
