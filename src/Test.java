6038b2c82355588e95a6500963d1089fa32e174f
MohamedAbdelfattah022 <mhabdelfattah22@gmail.com>
2023-05-14 15:28:29 +0300

    created new branch

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
         Theatre t = new Theatre();
         Scanner sc = new Scanner(System.in);
-        t.ticketReservation();
+        System.out.println("Enter Seat Class:");
+        int seatClass = sc.nextInt();
+        t.ticketReservation(seatClass);
         System.out.println("First Class:");
-        // for (int i = 0; i < 4; i++) {
-        // for (int j = 0; j < 5; j++) {
-        // System.out.print(t.getFirstClassSeats()[i][j] + " ");
-        // }
-        // System.out.println("\n");
-        // }
+        t.printArray(t.getFirstClassSeats(), 4);
+
+        System.out.println("Enter Seat to Cancel -1 to exit:");
+        int seat = sc.nextInt();
+        while (seat != -1) {
+            System.out.println("Enter Seat to Cancel -1 to exit:");
+            t.cancelReservation(seatClass, seat, t.getFirstClassSeats(), 4);
+            t.printArray(t.getFirstClassSeats(), 4);
+            seat = sc.nextInt();
+        }
+
+        System.out.println("Total cost: $" + t.getTotalCost());
+        t.resetReservations();
+        t.printArray(t.getFirstClassSeats(), 4);
+
         // System.out.println("Second Class:");
         // for (int i = 0; i < 8; i++) {
         // for (int j = 0; j < 5; j++) {
@@ -26,6 +37,7 @@ public class Test {
         // }
         // System.out.println("\n");
         // }
+
         sc.close();
     }
 }
